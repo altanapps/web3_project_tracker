5f72abed7a0ac284b5ba2ce99ca159efc435cad9
altanapps <futureisaltan@gmail.com>
2022-12-26 12:12:00 +0000

    implemented the ability to pull Github projects

--- src/find_projects.py
+++ src/find_projects.py
@@ -1,4 +1,15 @@
-import scrape
+import time
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+
+CHROME_DRIVER_PATH = '/Users/altantutar/Desktop/chromedriver'
+HOMEPAGE = "https://github.com/topics/blockchain"
+LOAD_MORE = "/html/body/div[1]/div[4]/main/div[2]/div[2]/div/div[1]/form/button"
+ARTICLES = "/html/body/div[1]/div[5]/main/div[2]/div[2]/div/div[1]/article[1]"
+NUM_ARTICLES = 20
+SLEEP_SECONDS = 1
+
 
 class Project:
     def __init__(self, time_stamp, url, stars):
@@ -8,4 +19,47 @@ class Project:
         self.stars = stars
 
 
-scrape.scrape_page("https://github.com/ethereum/go-ethereum")
+def write(project_links):
+    with open('data/project_links.txt', 'w') as f:
+        for file in project_links:
+            f.write(file + "\n")
+
+
+def save_results(driver):
+    # You should have loaded all the items now,
+    # download all the links
+    project_links = driver.find_elements_by_xpath(
+        './/article/div/div/div/h3/a[2]')
+    project_links = [link.get_attribute('href') for link in project_links]
+    print(len(project_links))
+
+    # Write all these projects into a textfile
+    write(project_links)
+
+
+def find_all_projects():
+    #############################
+    # LOADS ONLY 1,000 Projects #
+    #############################
+
+    # Set up the browser options
+    browser_options = ChromeOptions()
+    # browser_options.headless = True
+    driver = Chrome(executable_path=CHROME_DRIVER_PATH,
+                    options=browser_options)
+    driver.get(HOMEPAGE)
+    loaded = NUM_ARTICLES
+    while True:
+        try:
+            # Find the button and click it to get more data
+            driver.find_element_by_xpath(LOAD_MORE).click()
+            time.sleep(SLEEP_SECONDS)
+            loaded += NUM_ARTICLES
+        except NoSuchElementException:
+            save_results(driver)
+        except:
+            continue
+    save_results(driver)
+
+
+find_all_projects()
