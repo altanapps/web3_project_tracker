54af927d11ae7d8a0205e82d4ad396cdb8c74487
altanapps <futureisaltan@gmail.com>
2022-12-28 18:56:10 +0000

    setting up the lambda capability for the scraper

---
+++ src/lambda_function.py
@@ -0,0 +1,8 @@
+
+
+def lambda_handler(event, context):
+    # This function needs to do the following every six hours:
+    # 1) It will scrape and save the 1,000 top projects on Github
+    # 2) Then, it will get the desired information for all these projects
+    # 3) Finally, it will push the desired data to the database
+    return
